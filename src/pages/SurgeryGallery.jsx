f640fbc9598af58f5ffc8c45772d419db8be6b77
shobhitm1 <shobhitm182@gmail.com>
2026-03-21 16:05:12 +0530

    updated code for images section

--- src/pages/SurgeryGallery.jsx
+++ src/pages/SurgeryGallery.jsx
@@ -1,44 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SurgeryGallery.css";
 
-import img1 from "../assets/surgery1.jpeg";
-import img2 from "../assets/surgery2.jpeg";
-import img3 from "../assets/surgery3.jpeg";
-import img4 from "../assets/surgery4.jpeg";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
 
-function SurgeryGallery({ language }) {
+import s1 from "../assets/surgery1.jpeg";
+import s2 from "../assets/surgery2.jpeg";
+import s3 from "../assets/surgery3.jpeg";
+import s4 from "../assets/surgery4.jpeg";
+import s5 from "../assets/surgery5.jpeg";
 
-  const images = [img1, img2, img3, img4];
+function SurgeryGallery() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const images = [s1, s2, s3, s4, s5];
 
   return (
     <section className="surgery-section">
       <div className="container">
+        <h2 className="section-title">Surgery Highlights</h2>
 
-        <h2 className="section-title">
-          {language === "en"
-            ? "Our Surgical Expertise"
-            : "हमारी सर्जिकल विशेषज्ञता"}
-        </h2>
+        {/* 🔥 SLIDER BACK */}
+        <Swiper
+          modules={[Pagination, Autoplay]}
+          spaceBetween={20}
+          pagination={{ clickable: true }}
+          autoplay={{ delay: 2500 }}
+          loop={true}
+          breakpoints={{
+            0: { slidesPerView: 1 },
+            600: { slidesPerView: 2 },
+            1024: { slidesPerView: 3 }
+          }}
+        >
+          {images.map((img, index) => (
+            <SwiperSlide key={index}>
+              <img
+                src={img}
+                alt="surgery"
+                className="surgery-img"
+                onClick={() => setSelectedImage(img)}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
 
-        <div className="slider">
-
-          <div className="slide-track">
-            {images.map((img, index) => (
-              <div className="slide" key={index}>
-                <img src={img} alt="Surgery" />
-              </div>
-            ))}
-
-            {/* duplicate for smooth infinite loop */}
-            {images.map((img, index) => (
-              <div className="slide" key={`dup-${index}`}>
-                <img src={img} alt="Surgery" />
-              </div>
-            ))}
+        {/* 🔥 MODAL (ZOOM) */}
+        {selectedImage && (
+          <div
+            className="image-modal"
+            onClick={() => setSelectedImage(null)}
+          >
+            <img src={selectedImage} alt="preview" />
           </div>
-
-        </div>
-
+        )}
       </div>
     </section>
   );
